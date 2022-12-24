84a6ebf9885591192ceba5f092756962fafdaddc
David Nakov <nakov_david@mail.bg>
2022-12-24 14:18:00 +0200

    feat: added food generator class which generates a food at a random place in the field;

---
+++ Workshops/Snake Game/Snake Game/Models/Food/FoodGenerator.cs
@@ -0,0 +1,28 @@
+﻿namespace SnakeGame.Models.Food
+{
+    using Contracts;
+
+    public class FoodGenerator
+    {
+        private Random random;
+
+        private const int min = 0;
+
+        private int xMax = Console.WindowWidth;
+
+        private int yMax = Console.WindowHeight;
+
+        public FoodGenerator()
+        {
+            random = new Random();
+        }
+
+        public IFood Generate()
+        {
+            var foodXCoordinates = random.Next(min, xMax);
+            var foodYCoordinates = random.Next(min, yMax);
+            var food = new NormalFood(foodXCoordinates, foodYCoordinates);
+            return food;
+        }
+    }
+}
